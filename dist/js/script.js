71e178f26157d306b9d32db282b32b7dc52999be
Benjamin <benjamin.gosset.pro@gmail.com>
2020-10-06 18:58:09 +0200

    Mobile front-end done

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,9 +1,10 @@
 const navBtn = document.querySelector('.nav__btn');
 const navList = document.querySelector('nav ul');
 const nav = document.querySelector('nav');
+const main = document.querySelector('main');
 
-//navBtn.onclick = () => navList.classList.toggle('visible');
 navBtn.onclick = () => {
     navList.classList.toggle('visible');
     nav.classList.toggle('visible');
+    main.classList.toggle('menu-open');
 }
