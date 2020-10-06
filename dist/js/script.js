3c955d80fb36512afbd45f593af896009907a914
Benjamin <benjamin.gosset.pro@gmail.com>
2020-10-06 19:52:08 +0200

    Add slider

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,10 +1,17 @@
-const navBtn = document.querySelector('.nav__btn');
-const navList = document.querySelector('nav ul');
-const nav = document.querySelector('nav');
-const main = document.querySelector('main');
+$(document).ready(function(){
+    const navBtn = document.querySelector('.nav__btn');
+    const navList = document.querySelector('nav ul');
+    const nav = document.querySelector('nav');
+    const main = document.querySelector('main');
 
-navBtn.onclick = () => {
-    navList.classList.toggle('visible');
-    nav.classList.toggle('visible');
-    main.classList.toggle('menu-open');
-}
+    navBtn.onclick = () => {
+        navList.classList.toggle('visible');
+        nav.classList.toggle('visible');
+        main.classList.toggle('menu-open');
+    }
+
+    $('.slider__slides').slick({
+      prevArrow: '.slider__left-arrow',
+      nextArrow: '.slider__right-arrow'
+    });
+  });
